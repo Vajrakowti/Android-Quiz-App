ac3aeb0370fadcb95293274a275fe2f4ab8b2803
vajra <kowtilyavajra@gmail.com>
2024-02-28 17:29:41 +0530

    Final Commit

---
+++ app/src/main/java/com/example/quiza/QuestionData.kt
@@ -0,0 +1,13 @@
+package com.example.quiza
+
+data class QuestionData (
+
+    var id:Int,
+    var question:String,
+
+    var option_one:String,
+    var option_tw0:String,
+    var option_three:String,
+    var option_four:String,
+    var correct_ans:Int
+)
